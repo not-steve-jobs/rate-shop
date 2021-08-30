2d995e64c71b623cf7dc11cbf02f4939392e3fee
vahe-git <hovhannisyanvahe17@gmail.com>
2021-08-30 13:03:36 +0400

    Cron-Node

--- controller/cba-request.js
+++ controller/cba-request.js
@@ -1,12 +1,11 @@
 const {logger} = require('../utils/logger');
 const fetch = require('node-fetch');
 const rateModel = require('../models/DB_associations').Rate;
-
-//REDIS
+const cron = require('node-cron');
 const redis = require('redis');
 const client = redis.createClient(6379);
 client.on('connect', () => {
-    logger.info('Connected!', 8888888888);
+    logger.info('Connected!');
 });
 client.on("error", err => {
     logger.error(err);
@@ -28,9 +27,9 @@ const body = '<?xml version="1.0" encoding="utf-8"?>\n' +
         '  </soap:Body>\n' +
         '</soap:Envelope>';
 
-const cbaRequest = async (req, res) => {
-    logger.info('Request to CBA Started - - -');
 
+cron.schedule('0 0 0 * * *',  cbaRequest = async (req, res)=>{
+    logger.info('Request to CBA Started - - -');
     await fetch('http://api.cba.am/exchangerates.asmx?op=ExchangeRatesLatest', {
         method: 'post',
         body:    body,
@@ -41,15 +40,14 @@ const cbaRequest = async (req, res) => {
     })
         .then(res => res.text())
         .then(rate => {
-            logger.info('Request to CBA Finished - - -')
-
+            logger.info('Request to CBA Finished - - -');
             async function  Rate_Value  (){
                 let arr = rate.split('<ExchangeRate>');
                 let valutArr = [];
                 let FirstIndex;
                 let LastIndex;
                 let RateArr = [];
-                let rateObj = {}
+                let rateObj = {};
                 arr.shift();
                 for (let i=0; i<arr.length; i++){
                     if (arr[i] === ''){
@@ -68,7 +66,7 @@ const cbaRequest = async (req, res) => {
                 const rateFormat = Object.entries(rateObj).map(rate => ({
                     iso: rate[0],
                     rateVal: rate[1]
-                }))
+                }));
 
                 //update rate data
                 Promise.all(
@@ -82,19 +80,85 @@ const cbaRequest = async (req, res) => {
                 //adding rate data
                 // const newRate = await rateModel.bulkCreate(rateFormat, { updateOnDuplicate: ['iso', 'rateVal'] })
 
-                console.log(today)
                 client.set(
                     "key",
                     JSON.stringify(rateObj)
                 );
             };
-
-
-
-            Rate_Value()
+            Rate_Value();
             return res.status(200).json({rate});
         });
-};
+    console.log('running a task every day 12 AM..');
+});
+
+
+
+// const cbaRequest = async (req, res) => {
+//     logger.info('Request to CBA Started - - -');
+//
+//     await fetch('http://api.cba.am/exchangerates.asmx?op=ExchangeRatesLatest', {
+//         method: 'post',
+//         body:    body,
+//         headers: {
+//             'Content-Type': 'text/xml',
+//             'SOAPAction': 'http://www.cba.am/ExchangeRatesByDate'
+//         },
+//     })
+//         .then(res => res.text())
+//         .then(rate => {
+//             logger.info('Request to CBA Finished - - -')
+//
+//             async function  Rate_Value  (){
+//                 let arr = rate.split('<ExchangeRate>');
+//                 let valutArr = [];
+//                 let FirstIndex;
+//                 let LastIndex;
+//                 let RateArr = [];
+//                 let rateObj = {}
+//                 arr.shift();
+//                 for (let i=0; i<arr.length; i++){
+//                     if (arr[i] === ''){
+//                         arr.splice(i, 1)
+//                     }
+//                 };
+//                 for (let i=0; i<arr.length; i++){
+//                     valutArr[i] = arr[i].substr(5, 3)
+//                     FirstIndex = arr[i].indexOf('<Rate>')
+//                     LastIndex = arr[i].indexOf('</Rate>')
+//                     RateArr[i] = arr[i].slice(FirstIndex+6, LastIndex)
+//                 };
+//                 for(let i = 0; i < valutArr.length; i++){
+//                     rateObj[valutArr[i]] = RateArr[i]
+//                 };
+//                 const rateFormat = Object.entries(rateObj).map(rate => ({
+//                     iso: rate[0],
+//                     rateVal: rate[1]
+//                 }))
+//
+//                 //update rate data
+//                 Promise.all(
+//                     rateFormat.map(async i=>{
+//                         await rateModel.update(
+//                             { rateVal: i.rateVal },
+//                             { where: { iso: i.iso }} ,
+//                         )
+//                     })
+//                 );
+//                 //adding rate data
+//                 // const newRate = await rateModel.bulkCreate(rateFormat, { updateOnDuplicate: ['iso', 'rateVal'] })
+//
+//                 client.set(
+//                     "key",
+//                     JSON.stringify(rateObj)
+//                 );
+//             };
+//
+//
+//
+//             Rate_Value()
+//             return res.status(200).json({rate});
+//         });
+// };
 
 module.exports = {
     cbaRequest
